95bd0b057c19a1c4ce2952553c8567aee8715fbd
Holland McTyeire <holland.mctyeire@gmail.com>
2019-07-21 15:40:03 -0400

    Sunday Changes

--- Taco-Bell.js
+++ Taco-Bell.js
@@ -3,6 +3,6 @@ const para = document.querySelector('button');
 para.addEventListener('click', updateName);
 
 function updateName() {
-  let name = prompt('Type In Your Favorite Item');
-  para.textContent = 'Type In Your Favorite Item: ' + name;
+  let name = prompt('What is your favorite taco bell item?');
+  para.textContent = name;
 }
--- readme.txt
+++ readme.txt
@@ -1,1 +1,2 @@
-This is my new repo
+This is my first repository. The project is designed to show the fundamentals of HTML, CSS, and Javascript. In addition, it is meant to prove my competency with Git, Github, and Visual Studio Code.
+The webpage highlights my love and knowledge of Taco Bell and requires no special instructions to use. 
