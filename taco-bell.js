c151d3d6d7cccfa41eade7ddcf2eda74d00ddefe
Holland McTyeire <holland.mctyeire@gmail.com>
2019-07-25 19:31:45 -0400

    remove extra js

--- taco-bell.js
+++
@@ -1,9 +0,0 @@
-const para = document.querySelector('button');
-
-para.addEventListener('click', updateName);
-
-function updateName() {
-  let name = prompt('What is your favorite taco bell item?');
-  para.textContent = name;
-}
-//Function is meant to allow the user to type in their favorite item in a button. The text in the button will change to reflect their input.
