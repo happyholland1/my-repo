3f7493269549139ae56eadf22b1a3b9c34988eb9
Holland McTyeire <holland.mctyeire@gmail.com>
2019-07-25 18:05:50 -0400

    Thursday changes

--- Taco-Bell.js
+++ Taco-Bell.js
@@ -6,3 +6,4 @@ function updateName() {
   let name = prompt('What is your favorite taco bell item?');
   para.textContent = name;
 }
+//Function is meant to allow the user to type in their favorite item in a button. The text in the button will change to reflect their input.
